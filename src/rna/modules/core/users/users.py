cdaf51128a3352b1e34b8fc46eeea85af3661edd
William Flores <will7207@gmail.com>
2020-12-18 15:16:05 -0800

    doc: update doc for User Management Base Class

--- src/rna/modules/core/users/users.py
+++ src/rna/modules/core/users/users.py
@@ -18,16 +18,37 @@ class AbstractUserManagement(ABC):
 
     @abstractmethod
     def get_user_list(self, options: UserFilterOptions) -> List[UserDetailView]:
+        """
+        Gets a list of users given
+        :param options: available filtering options
+
+        - username
+        - TBD
+        :returns: List of User View
+        """
         pass
 
     @abstractmethod
     def create_user(self, details: UserCreationSchema) -> UserDetailView:
+        """
+        Create A New User
+        :param details: details of user to be created
+        """
         pass
 
     @abstractmethod
     def delete_user(self, user_id):
+        """
+        Delete a User by Id
+        :param user_id: unique user identity or id
+        """
         pass
 
     @abstractmethod
     def update_user(self, user_id, details: UserUpdateSchema):
+        """
+        Update an Existing User
+        :param user_id: unique user identity or id
+        :param details: user update info
+        """
         pass
